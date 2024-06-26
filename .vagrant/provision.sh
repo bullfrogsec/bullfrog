77134d8212bbc2f3d4cb3bcea4ff76142a1f46b8
Francois Allard <francois.allard@ymail.com>
2024-06-25 23:30:11 -0400

    ci: Add release workflow (#9)

--- .vagrant/provision.sh
+++ .vagrant/provision.sh
@@ -12,3 +12,19 @@ apt-get install -y curl software-properties-common apt-utils jq golang
 # install Node.js 20.x
 curl -sL https://deb.nodesource.com/setup_20.x | bash -
 apt-get install -y nodejs
+
+# install docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
