e1a20a3e2a0c0641dff8ae34f92fb2e8acb722e9
Francois Allard <francois.allard@ymail.com>
2024-07-01 15:26:55 -0400

    fix: filtering dns server in agent instead of nftables (#45)
    Filtering in agent gives us better visibility for now, since we don't
    currently log blocked traffic from nftables

--- test/input.js
+++ test/input.js
@@ -5,7 +5,10 @@ process.env["INPUT_LOG-DIRECTORY"] = "/tmp/gha-agent/logs";
 process.env["INPUT_ALLOWED-DOMAINS"] = `
 *.google.com
 bing.com
+*.github.com
 `;
 
-// uncomment to test with local agent
+// uncomment to test with local agent or download release
 // process.env["INPUT_LOCAL-AGENT-PATH"] = "agent/agent";
+process.env["INPUT_AGENT-DOWNLOAD-BASE-URL"] =
+  "https://github.com/bullfrogsec/bullfrog/releases/download/";
