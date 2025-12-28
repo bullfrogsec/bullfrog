de79b7c0178b3197be8e77a88858378cc742bd99
Francois Allard <allardtech@gmail.com>
2025-12-27 23:10:08 -0500

    fix: add untrusted dns server to logs + fix integration tests

--- test/block.sh
+++ test/block.sh
@@ -6,7 +6,9 @@ set -x
 # Stop on failure
 set -e
 
-POST_WARNINGS_FILEPATH=/tmp/post.warnings
+sudo rm -f $GITHUB_STEP_SUMMARY
+export GITHUB_STEP_SUMMARY="/tmp/github_step_summary"
+touch $GITHUB_STEP_SUMMARY
 
 sudo pkill --signal 9 agent || true
 
@@ -16,7 +18,6 @@ sudo systemctl restart docker
 # TODO: remove all other files we install
 
 sudo rm -f /var/run/bullfrog/agent-ready
-sudo rm -f $POST_WARNINGS_FILEPATH
 
 sudo touch /etc/sudoers.d/runner
 
@@ -29,14 +30,13 @@ source /vagrant/test/make_dns_requests.sh
 
 NODE_OPTIONS=--enable-source-maps node \
   --require /vagrant/test/block.env.js \
-  /vagrant/action/dist/post.js | grep "^::warning::" |  sed 's/%0A/\n/g' > $POST_WARNINGS_FILEPATH
+  /vagrant/action/dist/post.js
 
-grep --quiet 'Blocked DNS request to www.bing.com from unknown process' $POST_WARNINGS_FILEPATH
-grep --quiet 'Blocked request to 93.184.215.14:443 from processs `/usr/bin/curl https://93.184.215.14 --output /dev/null' $POST_WARNINGS_FILEPATH
-grep --quiet 'Blocked DNS request to registry-1.docker.io from unknown process' $POST_WARNINGS_FILEPATH
-grep --quiet 'Blocked DNS request to www.wikipedia.org from unknown process' $POST_WARNINGS_FILEPATH
-grep --quiet 'Blocked DNS request to tcp.example.com from unknown process' $POST_WARNINGS_FILEPATH
-grep --quiet 'Blocked request to www.google.com (8.8.8.8:53) from process `/usr/bin/dig @8.8.8.8 www.google.com`' $POST_WARNINGS_FILEPATH
-grep --quiet 'Blocked request to www.google.com (8.8.8.8:53) from process `/usr/bin/dig @8.8.8.8 www.google.com +tcp`' $POST_WARNINGS_FILEPATH
+grep --quiet  'www.bing.com</td><td>-</td><td>53</td><td>🚫 Blocked' $GITHUB_STEP_SUMMARY
+grep --quiet '93.184.215.14</td><td>443</td><td>🚫 Blocked' $GITHUB_STEP_SUMMARY
+grep --quiet  'registry-1.docker.io</td><td>-</td><td>53</td><td>🚫 Blocked' $GITHUB_STEP_SUMMARY
+grep --quiet  'www.wikipedia.org</td><td>-</td><td>53</td><td>🚫 Blocked' $GITHUB_STEP_SUMMARY
+grep --quiet  'tcp.example.com</td><td>-</td><td>53</td><td>🚫 Blocked' $GITHUB_STEP_SUMMARY
+grep --quiet  'www.google.com</td><td>8.8.8.8</td><td>53</td><td>🚫 Blocked' $GITHUB_STEP_SUMMARY
 
 echo "Tests passed successfully"
